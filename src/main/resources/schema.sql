dbfcc693545e0d7d36732560cfedb8987837e539
BYEONGRYEOL <lyulbyung@gmail.com>
2024-01-22 23:09:02 +0900

    Update: db
    사용되지 않는 ddl문 제거
    
    Ref: #1

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,46 +1,30 @@
-
 CREATE TABLE member
 (
     id         BIGINT PRIMARY KEY AUTO_INCREMENT,
     email      VARCHAR(255) NOT NULL UNIQUE,
     password   VARCHAR(255) NOT NULL,
-    login_id     VARCHAR(255) NOT NULL UNIQUE,
+    login_id   VARCHAR(255) NOT NULL UNIQUE,
     nickname   VARCHAR(255) NOT NULL UNIQUE,
     stream_key VARCHAR(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE member_channel
 (
-    id                  BIGINT PRIMARY KEY AUTO_INCREMENT,
-    title               VARCHAR(255) NOT NULL UNIQUE,
-    member_id           BIGINT NOT NULL UNIQUE,
-    streamer_nickname   VARCHAR(255) NOT NULL,
-    total_streaming     INTEGER NOT NULL,
-    started_at          TIMESTAMP,
-    on_air              BOOLEAN NOT NULL
-);
-
-CREATE TABLE point
-(
-    id                  SERIAL PRIMARY KEY,
-    member_id           BIGINT NOT NULL UNIQUE,
-    point               INTEGER,
-    nickname            VARCHAR(255) NOT NULL
+    id              BIGINT PRIMARY KEY AUTO_INCREMENT,
+    title           VARCHAR(255) NOT NULL UNIQUE,
+    member_id       BIGINT       NOT NULL UNIQUE,
+    total_streaming INTEGER      NOT NULL,
+    started_at      TIMESTAMP,
+    on_air          BOOLEAN      NOT NULL
 );
 
 CREATE TABLE point_log
 (
-    id                  SERIAL PRIMARY KEY,
-    streamer_id         BIGINT NOT NULL,
-    donater_id          BIGINT NOT NULL,
-    contents            VARCHAR(255) NOT NULL,
-    donated_at          TIMESTAMP NOT NULL,
-    donate_point        INTEGER NOT NULL
+    id           SERIAL PRIMARY KEY,
+    streamer_id  BIGINT       NOT NULL,
+    donater_id   BIGINT       NOT NULL,
+    contents     VARCHAR(255) NOT NULL,
+    donated_at   TIMESTAMP    NOT NULL,
+    donate_point INTEGER      NOT NULL
 );
 
-
-ALTER TABLE member_channel
-    ADD started_at DATETIME;
-
-ALTER TABLE member_channel
-    DROP streamer_nickname;
