225377288c474f35cc1995b8b597807741200a8e
BYEONGRYEOL <lyulbyung@gmail.com>
2024-01-23 17:17:13 +0900

    Update: DonationResponseDto
    시청자가 donation 후 리렌더링 되어야할 정보는 잔액 밖에 없음
    
    Ref: #4

--- src/main/java/com/hanghae/lemonairservice/dto/point/DonationResponseDto.java
+++ src/main/java/com/hanghae/lemonairservice/dto/point/DonationResponseDto.java
@@ -4,20 +4,9 @@ import lombok.Getter;
 
 @Getter
 public class DonationResponseDto {
-	private Long donaterId;
-	private Long streamerId;
-	private String donaterNickname;
-	private String content;
 	private int remainingPoint;
-	private String donated;
 
-	public DonationResponseDto(Long donaterId, String nickname, Long streamerId, String contents, int remainingPoint,
-		String now) {
-		this.donaterId = donaterId;
-		this.streamerId = streamerId;
-		this.donaterNickname = nickname;
-		this.content = contents;
+	public DonationResponseDto(int remainingPoint) {
 		this.remainingPoint = remainingPoint;
-		this.donated = now;
 	}
 }
