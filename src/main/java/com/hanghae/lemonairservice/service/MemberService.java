4fe46b639621ac65921152d6f36276b7c30e7933
munee <121735319+alaneelee@users.noreply.github.com>
2024-01-25 17:15:54 +0900

    Refactor: "Refactoring signup, login, validatePassword method"
    #1

--- src/main/java/com/hanghae/lemonairservice/service/MemberService.java
+++ src/main/java/com/hanghae/lemonairservice/service/MemberService.java
@@ -31,7 +31,6 @@ import reactor.core.publisher.Mono;
 @Slf4j
 @RequiredArgsConstructor
 public class MemberService {
-
 	private static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$";
 	private static final Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
 	private final MemberRepository memberRepository;
