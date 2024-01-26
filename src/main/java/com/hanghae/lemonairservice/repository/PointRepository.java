f798f0a9a6617ad8b58bc31a8d6881863b235009
BYEONGRYEOL <lyulbyung@gmail.com>
2024-01-26 16:56:12 +0900

    Feat: 잔액 조회 쿼리
    Ref #4

--- src/main/java/com/hanghae/lemonairservice/repository/PointRepository.java
+++ src/main/java/com/hanghae/lemonairservice/repository/PointRepository.java
@@ -1,11 +1,13 @@
 package com.hanghae.lemonairservice.repository;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.r2dbc.repository.Query;
 
 import com.hanghae.lemonairservice.entity.Point;
 
 import reactor.core.publisher.Mono;
 
 public interface PointRepository extends ReactiveCrudRepository<Point, Long> {
-	Mono<Point> findByMemberId(Long id);
+	@Query(value = "select sum( case when p.donator_id = :memberId then -point when p.member_id = :memberId then point else 0 end ) from point p")
+	Mono<Integer> totalPoint(Long memberId);
 }
